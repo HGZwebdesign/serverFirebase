63448f24b649e501c3ef8fb9aad262e3554f5827
Hanna <HGZwebdesign@gmail.com>
2020-06-20 17:20:38 +0200

    arch: example added

---
+++ arch/database/sushi.js
@@ -0,0 +1,39 @@
+module.exports = {
+  sushi: [
+    {
+      name: "Nigiri",
+      description:
+        "Nigiri to najstarsza forma sushi i najbardziej lubiana przez znawców i smakoszy sushi. Jest to uformowany, owalny kawałek ryżu z odrobiną wasabi, na wierzchu którego znajduje się np. surowa ryba, omlet japoński lub awokado. Nazwa nigiri pochodzi od słowa nigiru, co oznacza ściskać, ugniatać.",
+      size: 2,
+      id: 1,
+    },
+    {
+      name: "Hosomaki",
+      description:
+        "Hosomaki są to cienkie rolki z niewielką ilością ryżu i dodatkiem tylko jednego składnika. Najczęściej są to różnego rodzaju ryby bądź warzywa. Nazwa hosomaki pochodzi od przymiotnika hosoi oznaczającego cienki, chudy. Rolka sushi po zwinięciu krojona jest na sześć kawałków.",
+      size: 1,
+      id: 2,
+    },
+    {
+      name: "Futomaki",
+      description:
+        "Futomaki są to grube rolki z minimum czterema składnikami – od ryb i owoców morza po świeże warzywa. Nazwa pochodzi od przymiotnika futoi, oznaczającego gruby, tłusty. Jest to jeden z najbardziej popularnych rodzajów sushi.",
+      size: 2,
+      id: 3,
+    },
+    {
+      name: "Uramaki (California maki)",
+      description:
+        "Uramaki są to rolki zawijane odwrotnie, czyli z ryżem na zewnątrz i glonami nori wewnątrz. Zewnętrzna warstwa ryżu często posypana jest sezamem. Ura, po japońsku oznacza odwrotnie, a maki – rolować. Wymyślone zostały na rynek amerykański. Ich bardziej potoczna nazwa to california maki.",
+      size: 2,
+      id: 4,
+    },
+    {
+      name: "Sashimi roll",
+      description:
+        "Sashimi roll to sushi bez ryżu. Są to kawałki surowej i świeżej ryby wraz z warzywami zawinięte w papier ryżowy.",
+      size: 2,
+      id: 5,
+    },
+  ],
+};
